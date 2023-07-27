650a3ace8e7baa88d687016b67716fcbbca29684
Mary <mary.vardanyan.2001@gmail.com>
2023-07-27 10:35:27 +0400

    components styles, index

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles/index.scss";
 import "@fontsource/montserrat";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
